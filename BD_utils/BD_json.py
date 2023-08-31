37a33d8a5fef04021b96f486d777f5914e54d439
Leonardo Bazilio <leobazao@hotmail.com>
2023-08-30 17:04:36 -0700

    fix format problems in bd_json utils file

--- BD_utils/BD_json.py
+++ BD_utils/BD_json.py
@@ -1,5 +1,6 @@
 import json
 
+
 def write_json_file(json_file, data, sort_dic=False):
     """Saves object (dictionary) into json file"""
     with open(json_file, 'w') as fp:
@@ -10,12 +11,12 @@ def write_json_file(json_file, data, sort_dic=False):
             return False
     return True
 
+
 def read_json_file(file):
+    content = None
+    with open(file, 'r') as fp:
+        content = json.loads(fp.read())
 
-	content = None
-	with open(file, 'r') as fp:
-		content = json.loads(fp.read())
+    return content
 
-	return content
-
-#TODO: Criar um update_json_file
+# TODO: Criar um update_json_file
